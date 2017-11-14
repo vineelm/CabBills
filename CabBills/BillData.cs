d597a7d87f7d24f45dba74d6cef68b56059b1fda
Vineel <vineelmada@gmail.com>
2017-11-14 20:06:20 +0530

    Initial

---
+++ CabBills/BillData.cs
@@ -0,0 +1,26 @@
+﻿namespace CabBills
+{
+    class BillData
+    {
+        public string Date { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string BaseFare { get; set; }
+        public string DistanceKms { get; set; }
+        public string DistanceFare { get; set; }
+        public string RideTime { get; set; }
+        public string RideFare { get; set; }
+
+        public string AdvanceBooking { get; set; }
+        public string Taxes { get; set; }
+        public string TotalBill { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string SourceAddress { get; set; }
+        public string DestinationAddress { get; set; }
+
+
+
+
+    }
+}
